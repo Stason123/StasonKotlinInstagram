66a0be0fb7ae8e46af903c1eebfda6e0577a4d32
stasonnoris321 <stasonnoris321@gmail.com>
2020-01-27 20:00:14 +0200

    changes

--- app/src/main/java/com/e/instagramstasonkotlin/MainActivity.kt
+++ app/src/main/java/com/e/instagramstasonkotlin/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : BaseActivity(0) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        setContentView(R.layout.activity_home)
         Log.d(TAG, "onCreate")
         setupBottomNavigation()
 
