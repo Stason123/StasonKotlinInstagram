4b12ffa59fd54597594026b310c36688c1d92c9d
stasonnoris321 <stasonnoris321@gmail.com>
2020-02-04 18:53:56 +0200

    changes

--- app/src/main/java/com/e/instagramstasonkotlin/MainActivity.kt
+++ app/src/main/java/com/e/instagramstasonkotlin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.e.instagramstasonkotlin
 import android.os.Bundle
 import android.util.Log
 import com.alexbezhan.instagram.activities.BaseActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : BaseActivity(0) {
 
@@ -13,6 +14,16 @@ class MainActivity : BaseActivity(0) {
         setContentView(R.layout.activity_home)
         Log.d(TAG, "onCreate")
         setupBottomNavigation()
+
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword("stasonnoris321@gmail.com", "123456789")
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.d(TAG, "signIn: Successful")
+                } else {
+                    Log.e(TAG, "signIn: Failed", it.exception)
+                }
+            }
 
     } 
 }
