6ac36d9750c327fc1b0e748756d94090011ba486
stasonnoris321 <stasonnoris321@gmail.com>
2020-02-10 19:03:55 +0200

    Changes 19

--- app/src/main/java/com/e/instagramstasonkotlin/activities/EditProfileActivity.kt
+++ app/src/main/java/com/e/instagramstasonkotlin/activities/EditProfileActivity.kt
@@ -1,137 +1,80 @@
 package com.e.instagramstasonkotlin.activities
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
-import android.text.Editable
 import android.util.Log
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.core.content.FileProvider
-import com.bumptech.glide.Glide
 import com.e.instagramstasonkotlin.Models.User
 import com.e.instagramstasonkotlin.R
+import com.e.instagramstasonkotlin.utils.CameraPictureTaker
+import com.e.instagramstasonkotlin.utils.FirebaseHelper
+import com.e.instagramstasonkotlin.utils.ValueEventListenerAdapter
 import com.e.instagramstasonkotlin.views.PasswordDialog
-import com.google.firebase.auth.AuthCredential
 import com.google.firebase.auth.EmailAuthProvider
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.*
-import com.google.firebase.storage.FirebaseStorage
-import com.google.firebase.storage.StorageReference
 import kotlinx.android.synthetic.main.activity_edit_profile.*
-import java.io.File
-import java.text.SimpleDateFormat
-import java.util.*
 
 class EditProfileActivity : AppCompatActivity(), PasswordDialog.Listener {
     private val TAG =  "EditProfileActivity"
     private lateinit var mUser: User
     private lateinit var mPendingUser: User
-    private lateinit var mAuth: FirebaseAuth
-    private lateinit var mDatabase: DatabaseReference
-    private lateinit var mStorage: StorageReference
-    private val TAKE_PICTURE_REQUEST_CODE = 1
-    private lateinit var mImageUri: Uri
-    val simpleDateFormat = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US)
+    private lateinit var mFirebaseHelper: FirebaseHelper
+    private lateinit var cameraPictureTaker: CameraPictureTaker
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
         Log.d(TAG, "onCreate")
+
+        cameraPictureTaker = CameraPictureTaker(this)
 
         close_image.setOnClickListener {
             finish()
         }
 
         save_image.setOnClickListener { updateProfile() }
-        change_photo_text.setOnClickListener { takeCameraPicture() }
+        change_photo_text.setOnClickListener { cameraPictureTaker.takeCameraPicture() }
 
-        mAuth = FirebaseAuth.getInstance()
-        //val user = auth.currentUser
-        mDatabase = FirebaseDatabase.getInstance().reference
-        mStorage = FirebaseStorage.getInstance().reference
-        mDatabase.child("users").child(mAuth.currentUser!!.uid)
+        mFirebaseHelper = FirebaseHelper(this)
+
+        mFirebaseHelper.currentUserReference()
             .addListenerForSingleValueEvent(ValueEventListenerAdapter {
                 mUser = it.getValue(User::class.java)!!
-                name_input.setText(mUser.name, TextView.BufferType.EDITABLE)
-                username_input.setText(mUser.username, TextView.BufferType.EDITABLE)
-                website_input.setText(mUser.website, TextView.BufferType.EDITABLE)
-                bio_input.setText(mUser.bio, TextView.BufferType.EDITABLE)
-                email_input.setText(mUser.email, TextView.BufferType.EDITABLE)
-                phone_input.setText(mUser.phone?.toString(), TextView.BufferType.EDITABLE)
+                name_input.setText(mUser.name)
+                username_input.setText(mUser.username)
+                website_input.setText(mUser.website)
+                bio_input.setText(mUser.bio)
+                email_input.setText(mUser.email)
+                phone_input.setText(mUser.phone?.toString())
                 profile_image.loadUserPhoto(mUser.photo)
 
         })
     }
 
-
-    private fun takeCameraPicture() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        if (intent.resolveActivity(packageManager) != null) {
-            val imageFile = createImageFile()
-            mImageUri = FileProvider.getUriForFile(this,
-                "com.e.instagramstasonkotlin.fileprovider",
-                imageFile)
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageUri)
-            startActivityForResult(intent, TAKE_PICTURE_REQUEST_CODE)
-        }
-    }
-
-    private fun createImageFile(): File {
-        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        return File.createTempFile(
-            "JPEG_${simpleDateFormat.format(Date())}_",
-            ".jpg",
-            storageDir
-        )
-    }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        //super.onActivityResult(requestCode, resultCode, data)
-                if (requestCode == TAKE_PICTURE_REQUEST_CODE && resultCode == RESULT_OK) {
-            val uid = mAuth.currentUser!!.uid
-            var ref = mStorage.child("users/$uid/photo")
-                ref.putFile(mImageUri).addOnCompleteListener {
-                if (it.isSuccessful) {
-                    ref.downloadUrl.addOnCompleteListener {
-                        val photoUrl = it.result.toString()
-                        mDatabase.child("users/$uid/photo").setValue(photoUrl)
-                            .addOnCompleteListener {
-                                if (it.isSuccessful) {
-                                    mUser = mUser.copy(photo = photoUrl)
-                                    profile_image.loadUserPhoto(mUser.photo)
-                                    Log.d(TAG, "onActivityResult: photo saved successfully $photoUrl")
-                                } else {
-                                    showToast(it.exception!!.message!!)
-                                }
-                            }
+        if (requestCode == cameraPictureTaker.REQUEST_CODE && resultCode == RESULT_OK) {
+            mFirebaseHelper.uploadUserPhoto(cameraPictureTaker.imageUri!!) {
+                var ref = mFirebaseHelper.getUploadPath()
+                ref.downloadUrl.addOnCompleteListener {
+                    val photoUrl = it.result.toString()
+                    mFirebaseHelper.updateUserPhoto(photoUrl) {
+                        mUser = mUser.copy(photo = photoUrl)
+                        profile_image.loadUserPhoto(mUser.photo)
                     }
-
-                } else {
-                    showToast(it.exception!!.message!!)
                 }
+
             }
         }
     }
 
-
     private fun updateProfile() {
-        // get user from input
         mPendingUser = readInputs()
-        // validate inputs
         val error = validate(mPendingUser)
         if(error == null) {
-            //save
             if (mPendingUser.email ==  mUser.email) {
                 updateUser(mPendingUser)
             } else {
                 PasswordDialog().show(supportFragmentManager, "password_dialog")
-                //re-authenticate
-                //update email in auth
-                //update user
             }
         } else {
             showToast(error)
@@ -149,7 +92,19 @@ class EditProfileActivity : AppCompatActivity(), PasswordDialog.Listener {
         )
     }
 
+    override fun onPasswordConfirm(password: String) {
+        if (password.isNotEmpty()) {
+            val credential = EmailAuthProvider.getCredential(mUser.email, password)
+            mFirebaseHelper.reauthenticate(credential) {
+                mFirebaseHelper.updateEmail(mPendingUser.email) {
+                    updateUser(mPendingUser)
+                }
+            }
+        } else {
+            showToast("You should enter your password")
+        }
 
+    }
 
     private fun updateUser(user: User) {
         val updatesMap = mutableMapOf<String, Any?>()
@@ -160,7 +115,7 @@ class EditProfileActivity : AppCompatActivity(), PasswordDialog.Listener {
         if (user.email != mUser.email) updatesMap["email"] = user.email
         if (user.phone != mUser.phone) updatesMap["phone"] = user.phone
 
-        mDatabase.updateUser(mAuth.currentUser!!.uid, updatesMap) {
+        mFirebaseHelper.updateUser(updatesMap) {
             showToast("Profile saved")
             finish()
         }
@@ -173,51 +128,5 @@ class EditProfileActivity : AppCompatActivity(), PasswordDialog.Listener {
             user.email.isEmpty() -> "Please enter email"
             else -> null
         }
-
-    override fun onPasswordConfirm(password: String) {
-        if (password.isNotEmpty()) {
-            val credential = EmailAuthProvider.getCredential(mUser.email, password)
-            mAuth.currentUser!!.reauthenticate(credential) {
-                mAuth.currentUser!!.updateEmail(mPendingUser.email) {
-                    updateUser(mPendingUser)
-                }
-            }
-        } else {
-            showToast("You should enter your password")
-        }
-
-    }
-
-    private fun DatabaseReference.updateUser(uid: String, updates: Map<String, Any?>,
-                                             onSuccess: () -> Unit) {
-        child("users").child(uid).updateChildren(updates)
-            .addOnCompleteListener {
-                if (it.isSuccessful) {
-                    onSuccess()
-                } else {
-                    showToast(it.exception!!.message!!)
-                }
-            }
-    }
-
-    private fun FirebaseUser.updateEmail(email: String, onSuccess: () -> Unit) {
-        updateEmail(email).addOnCompleteListener {
-            if (it.isSuccessful) {
-                onSuccess()
-            } else {
-                showToast(it.exception!!.message!!)
-            }
-        }
-    }
-
-    private fun FirebaseUser.reauthenticate(credential: AuthCredential, onSuccess: () -> Unit) {
-        reauthenticate(credential).addOnCompleteListener {
-            if (it.isSuccessful) {
-                onSuccess()
-            } else {
-                showToast(it.exception!!.message!!)
-            }
-        }
-    }
 
 }
--- app/src/main/java/com/e/instagramstasonkotlin/activities/Utils.kt
+++ app/src/main/java/com/e/instagramstasonkotlin/activities/Utils.kt
@@ -18,20 +18,6 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 
-class ValueEventListenerAdapter(val handler: (DataSnapshot) -> Unit) : ValueEventListener {
-    private val TAG = "ValueEventListenerAdapter"
-
-    override fun onDataChange(data: DataSnapshot) {
-        handler(data)
-    }
-
-    @SuppressLint("LongLogTag")
-    override fun onCancelled(error: DatabaseError) {
-        Log.e(TAG, "onCenceled", error.toException())
-    }
-}
-@GlideModule
-class CustomGlideModule : AppGlideModule()
 
 fun Context.showToast(text: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, text, duration).show()
