0cbed940157cd85e72f2c0298423742e514f58aa
stasonnoris321 <stasonnoris321@gmail.com>
2020-02-10 22:30:29 +0200

    Change 20

--- app/src/main/java/com/e/instagramstasonkotlin/activities/ProfileActivity.kt
+++ app/src/main/java/com/e/instagramstasonkotlin/activities/ProfileActivity.kt
@@ -4,11 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import com.alexbezhan.instagram.activities.BaseActivity
+import com.e.instagramstasonkotlin.Models.User
 import com.e.instagramstasonkotlin.R
+import com.e.instagramstasonkotlin.utils.FirebaseHelper
+import com.e.instagramstasonkotlin.utils.ValueEventListenerAdapter
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : BaseActivity(4) {
     private val TAG = "ProfileActivity"
+    private lateinit var mFirebaseHelper: FirebaseHelper
+    private lateinit var mUser : User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
@@ -18,5 +24,12 @@ class ProfileActivity : BaseActivity(4) {
             val intent = Intent(this, EditProfileActivity::class.java)
             startActivity(intent)
         }
+
+        mFirebaseHelper = FirebaseHelper(this)
+        mFirebaseHelper.currentUserReference().addValueEventListener(ValueEventListenerAdapter{
+            mUser = it.getValue(User::class.java)!!
+            profile_image.loadUserPhoto(mUser.photo)
+            username_text.text = mUser.username
+        })
     }
 }
--- app/src/main/java/com/e/instagramstasonkotlin/activities/ShareActivity.kt
+++ app/src/main/java/com/e/instagramstasonkotlin/activities/ShareActivity.kt
@@ -1,16 +1,34 @@
 package com.e.instagramstasonkotlin.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import com.alexbezhan.instagram.activities.BaseActivity
+import com.bumptech.glide.Glide
 import com.e.instagramstasonkotlin.R
+import com.e.instagramstasonkotlin.utils.CameraPictureTaker
+import kotlinx.android.synthetic.main.activity_share.*
 
 class ShareActivity : BaseActivity(2) {
     private val TAG = "ShareActivity"
+    private lateinit var mCameraHelper: CameraPictureTaker
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_share)
         Log.d(TAG, "onCreate")
-        setupBottomNavigation()
+
+        mCameraHelper = CameraPictureTaker(this)
+        mCameraHelper.takeCameraPicture()
+
+        back_image.setOnClickListener { finish() }
+
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == mCameraHelper.REQUEST_CODE && resultCode == RESULT_OK) {
+            Glide.with(this).load(mCameraHelper.imageUri).centerCrop().into(post_image)
+        }
     }
 }
