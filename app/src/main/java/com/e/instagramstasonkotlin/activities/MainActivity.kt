bb2466f2449e056e44ea2efa8a4ea57d13ff0805
stasonnoris321 <stasonnoris321@gmail.com>
2020-02-11 16:28:30 +0200

    Changes 21

--- app/src/main/java/com/e/instagramstasonkotlin/activities/MainActivity.kt
+++ app/src/main/java/com/e/instagramstasonkotlin/activities/MainActivity.kt
@@ -5,43 +5,45 @@ import android.os.Bundle
 import android.util.Log
 import com.alexbezhan.instagram.activities.BaseActivity
 import com.e.instagramstasonkotlin.R
+import com.e.instagramstasonkotlin.utils.FirebaseHelper
+import com.e.instagramstasonkotlin.utils.ValueEventListenerAdapter
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 
 class MainActivity : BaseActivity(0) {
 
     private val TAG = "MainActivity"
-    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mFirebase: FirebaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         Log.d(TAG, "onCreate")
         setupBottomNavigation()
-
-        mAuth = FirebaseAuth.getInstance()
-//        auth.signInWithEmailAndPassword("stasonnoris321@gmail.com", "123456789")
-//            .addOnCompleteListener {
-//                if (it.isSuccessful) {
-//                    Log.d(TAG, "signIn: Successful")
-//                } else {
-//                    Log.e(TAG, "signIn: Failed", it.exception)
-//                }
-//            }
-
+        mFirebase = FirebaseHelper(this)
         sign_out_text.setOnClickListener{
-            mAuth.signOut()
+            mFirebase.auth.signOut()
         }
-        mAuth.addAuthStateListener {
+
+        mFirebase.auth.addAuthStateListener {
             if (it.currentUser == null) {
                 startActivity(Intent(this, LoginActivity::class.java))
                 finish()
             }
         }
+
+        mFirebase.database.child("feed-post").child(mFirebase.auth.currentUser!!.uid)
+            .addValueEventListener(ValueEventListenerAdapter{
+                val posts = it.children.map {it.getValue(FeedPost::class.java)!!}
+//                Log.d(TAG, "feedPost ${posts.joinToString("\n", "\n")}")
+//                Log.d(TAG, "feedPost ${posts.first().timestampDate()}")
+            })
+
     }
 
     override fun onStart(){
         super.onStart()
-        if (mAuth.currentUser == null) {
+        if (mFirebase.auth.currentUser == null) {
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
         }
--- app/src/main/java/com/e/instagramstasonkotlin/activities/ShareActivity.kt
+++ app/src/main/java/com/e/instagramstasonkotlin/activities/ShareActivity.kt
@@ -5,15 +5,21 @@ import android.os.Bundle
 import android.util.Log
 import com.alexbezhan.instagram.activities.BaseActivity
 import com.bumptech.glide.Glide
+import com.e.instagramstasonkotlin.Models.User
 import com.e.instagramstasonkotlin.R
 import com.e.instagramstasonkotlin.utils.CameraPictureTaker
 import com.e.instagramstasonkotlin.utils.FirebaseHelper
+import com.e.instagramstasonkotlin.utils.ValueEventListenerAdapter
+import com.google.firebase.database.ServerValue
 import kotlinx.android.synthetic.main.activity_share.*
+import org.w3c.dom.Comment
+import java.util.*
 
 class ShareActivity : BaseActivity(2) {
     private val TAG = "ShareActivity"
     private lateinit var mCamera: CameraPictureTaker
     private lateinit var mFirebase: FirebaseHelper
+    private lateinit var mUser: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +32,10 @@ class ShareActivity : BaseActivity(2) {
 
         back_image.setOnClickListener { finish() }
         share_text.setOnClickListener { share() }
+
+        mFirebase.currentUserReference().addValueEventListener(ValueEventListenerAdapter{
+            mUser = it.getValue(User::class.java)!!
+        })
 
     }
 
@@ -48,27 +58,52 @@ class ShareActivity : BaseActivity(2) {
             ref.putFile(imageUri).addOnCompleteListener{
                     if (it.isSuccessful) {
                         ref.downloadUrl.addOnCompleteListener {
-                            var res = it.result.toString()
-                            Log.d(TAG, "ref $res")
+                            val imageDownloadUrl = it.result.toString()
+                            Log.d(TAG, "ref $imageDownloadUrl")
                             mFirebase.database.child("images").child(uid).push()
-                                .setValue(it.result.toString())
-                                .addOnCompleteListener{
+                                .setValue(imageDownloadUrl)
+                                .addOnCompleteListener {
                                     if (it.isSuccessful) {
-                                        startActivity(Intent(this,
-                                            ProfileActivity::class.java))
-                                        finish()
+                                        mFirebase.database.child("feed-post").child(uid)
+                                            .push()
+                                            .setValue(mkFeedPost(uid, imageDownloadUrl))
+                                            .addOnCompleteListener{
+                                                if (it.isSuccessful) {
+                                                    startActivity(Intent(this,
+                                                        ProfileActivity::class.java))
+                                                    finish()
+                                                }
+                                            }
                                     } else {
                                         showToast(it.exception!!.message!!)
                                     }
                                 }
                         }
-
-
-
                     } else {
                         showToast(it.exception!!.message!!)
                     }
                 }
         }
     }
+    private fun mkFeedPost(uid: String, imageDownloadUrl: String): FeedPost{
+        Log.d(TAG, "$uid and $imageDownloadUrl")
+        return FeedPost(
+            uid = uid,
+            username = mUser.username,
+            image = imageDownloadUrl,
+            caption = caption_input.text.toString(),
+            photo = mUser.photo
+        )
+    }
 }
+
+
+
+data class FeedPost(val uid: String = "", val username : String = "",
+                    val image : String = "", val likeCount: Int  = 0, val comentCount: Int = 0,
+                    val caption: String = "", val comments: List<Comment> = emptyList(),
+                    val timestamp: Any = ServerValue.TIMESTAMP, val photo: String? = null) {
+    fun timestampDate(): Date = Date(timestamp as Long)
+}
+
+data class Comment(val uid: String, val username: String, val text: String)
