53e2b1b489c02fb907ff0a1e6f41104630250002
stasonnoris321 <stasonnoris321@gmail.com>
2020-02-13 22:34:46 +0200

    Changes 24

--- app/src/main/java/com/e/instagramstasonkotlin/activities/MainActivity.kt
+++ app/src/main/java/com/e/instagramstasonkotlin/activities/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.e.instagramstasonkotlin.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.StyleSpan
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.alexbezhan.instagram.activities.BaseActivity
@@ -14,7 +24,6 @@ import com.bumptech.glide.Glide
 import com.e.instagramstasonkotlin.R
 import com.e.instagramstasonkotlin.utils.FirebaseHelper
 import com.e.instagramstasonkotlin.utils.ValueEventListenerAdapter
-import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.feed_item.view.*
 
@@ -64,6 +73,28 @@ class MainActivity : BaseActivity(0) {
     }
 }
 
+//class FeedAdapter(private val posts: List<FeedPost>)
+//    : RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
+//
+//    class ViewHolder(val view: View) : RecyclerView.ViewHolder(view)
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+//        val view = LayoutInflater.from(parent.context)
+//            .inflate(R.layout.feed_item, parent, false)
+//        return ViewHolder(view)
+//    }
+//
+//    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+//        holder.view.post_image.loadImage(posts[position].image)
+//    }
+//
+//    override fun getItemCount() = posts.size
+//
+//    private fun ImageView.loadImage(image: String) {
+//        Glide.with(this).load(image).centerCrop().into(this)
+//    }
+//}
+
 class FeedAdapter(private val posts: List<FeedPost>)
     : RecyclerView.Adapter<FeedAdapter.ViewHolder>() {
 
@@ -75,13 +106,47 @@ class FeedAdapter(private val posts: List<FeedPost>)
         return ViewHolder(view)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.view.post_image.loadImage(posts[position].image)
+        val post = posts[position]
+        with(holder) {
+            view.user_photo_image.loadImage(post.photo)
+            view.username_text.text = post.username
+            view.post_image.loadImage(post.image)
+            if (post.likeCount == 0) {
+                view.likes_text.visibility = View.GONE
+            } else {
+                view.likes_text.visibility = View.VISIBLE
+                view.likes_text.text = "${post.likeCount} likes"
+            }
+            view.caption_text.setCaptionText(post.username, post.caption)
+        }
+    }
+
+    private fun TextView.setCaptionText(username: String, caption: String) {
+        val usernameSpannable = SpannableString(username)
+        usernameSpannable.setSpan(
+            StyleSpan(Typeface.BOLD), 0, usernameSpannable.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        usernameSpannable.setSpan(object: ClickableSpan() {
+            override fun onClick(widget: View) {
+                widget.context.showToast("Username is clicked")
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+
+            }
+        }, 0, usernameSpannable.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        text = SpannableStringBuilder().append(usernameSpannable).append(" ")
+            .append(caption)
+        movementMethod = LinkMovementMethod.getInstance()
     }
 
     override fun getItemCount() = posts.size
 
-    private fun ImageView.loadImage(image: String) {
+    private fun ImageView.loadImage(image: String?) {
         Glide.with(this).load(image).centerCrop().into(this)
     }
 }
